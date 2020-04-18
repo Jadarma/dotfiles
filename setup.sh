0a9b38112d4cc5390ea09d9c733c88dda2ab2c2b
Dan Cîmpianu <dancristiancimpianu@gmail.com>
2020-04-19 00:55:24 +0300

    Added primitive setup script and dependency list.

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+# -----------------------------------------------------------------------------
+# Jadarma's Dotfiles Install Script
+#
+# This script assumes you already have a minimal install of Arch Linux and you
+# are running it from the dotfile directory.
+# -----------------------------------------------------------------------------
+
+# Create user directories.
+DIRS=('.local' '.local/share' '.cache' '.config' '.ssh'
+  'desktop' 'docs' 'docs/templates' 'dl' 'public' 'media'
+  'media/music' 'media/pictures' 'media/pictures/screenshots' 'media/videos')
+
+for dir in $DIRS; do
+  mkdir -p "$HOME/$dir"
+done
+
+# Restrict permissions
+chmod -R 700 "$HOME"
+chmod -R 744 "$HOME/public"
+
+# Stow dotfiles.
+stow -v -t "$HOME" alacritty bspwm dunst font gtk hosts lock picom polybar \
+  resources rofi scripts sxhkd vis xdg xorg zsh
+
+# Create alias for .zprofile
+ln -sf "$HOME/.profile" "$HOME/.zprofile"
+
+# Source env variables for this install script as well
+# shellcheck source=$HOME/.profile
+. "$HOME/.profile"
+
+# Install dependencies
+echo "Installing dependencies.."
+sed -e '/^[[:space:]]*#/d' pkglist.txt |
+  sed -E "s/^([a-z0-9-]+)\s*#.*$/\1/" |
+  yay -S --needed -
