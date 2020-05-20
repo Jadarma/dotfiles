a6afad223322ab04ebc78c9d9d96f56d1ef2c3c3
Dan Cîmpianu <dancristiancimpianu@gmail.com>
2020-05-20 22:38:21 +0300

    Added exa as a replacement for ls.
    * Added a zsh plugin containing some aliases for exa.
    * Deleted color-ls.plugin.zsh as it is no longer useful.
    * Fixed unrelated exit command from shell substitution.

---
+++ zsh/.config/zsh/plugins/exa.plugin.zsh
@@ -0,0 +1,25 @@
+# -----------------------------------------------------------------------------
+# EXA Configurations
+#
+# Exa is a modern `ls` replacement written in Rust.
+# @see https://the.exa.website/
+# -----------------------------------------------------------------------------
+
+# Basic shorthands: l + a for hidden files, l for detail view, t for tree view.
+alias l='exa'
+alias la='l -a'
+alias ll='exa -l --group-directories-first --time-style=long-iso'
+alias lla='ll -a'
+alias lt='ll -T'
+alias lta='lt -a'
+
+# Shows a tree view of current git repo, filtering just the changed files.
+# Yes, I am aware of `git status`.
+function lg() {
+  REPO=$(git rev-parse --show-toplevel)
+  [ -z "$REPO" ] && return 1
+  lta -I ".git" --git --color=always "$REPO" | awk -e '$6 !~ /--|I/ { print }'
+}
+
+# Some color overrides.
+export EXA_COLORS="da=0"
