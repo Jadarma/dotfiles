2b84dd94f1e9b1c7da8307cdc798be6b3363ebc0
Dan Cîmpianu <dancristiancimpianu@gmail.com>
2020-06-21 21:44:41 +0300

    Added terminal swallowing feature.

---
+++ zsh/.config/zsh/plugins/swallow.plugin.zsh
@@ -0,0 +1,23 @@
+# -----------------------------------------------------------------------------
+# Window Swallowing
+#
+# For interactive sessions only, enables window swallowing, which hides the
+# terminal emulator while spawning another window, such is the case when
+# launching applications from the terminal.
+#
+# You can swallow any command, by calling it via the `swallow` function.
+# For commands you use often, consider defining them as an alias instead.
+# -----------------------------------------------------------------------------
+
+function swallow() {
+  ID=$(xdo id)
+  xdo hide
+  "$@" >/dev/null
+  STATUS=$?
+  xdo show "$ID"
+  return $STATUS
+}
+
+alias sxiv='swallow sxiv'
+alias idea='swallow idea'
+alias mpv='swallow mpv --player-operation-mode=pseudo-gui'
