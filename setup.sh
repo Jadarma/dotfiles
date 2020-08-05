e1237c386ba2d558e79e4c8e5a5b1ccdca07945b
Dan Cîmpianu <dancristiancimpianu@gmail.com>
2020-08-05 18:24:06 +0300

    Updated setup script.
    * First install packages, then stow.
    * Added missing directories to stow command.
    * Removed manual creation of user directories.
    * Removed redundant `chmod`.

--- setup.sh
+++ setup.sh
@@ -1,37 +1,32 @@
 #!/bin/zsh
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 # Jadarma's Dotfiles Install Script
 #
 # This script assumes you already have a minimal install of Arch Linux and you
 # are running it from the dotfile directory.
-# -----------------------------------------------------------------------------
-
-# Create user directories.
-DIRS=('.local' '.local/share' '.cache' '.config' '.ssh'
-  'desktop' 'docs' 'docs/templates' 'dl' 'public' 'media'
-  'media/music' 'media/pictures' 'media/pictures/screenshots' 'media/videos')
-
-for dir in $DIRS; do
-  mkdir -p "$HOME/$dir"
-done
-
-# Restrict permissions
-chmod -R 700 "$HOME"
-chmod -R 744 "$HOME/public"
-
-# Stow dotfiles.
-stow -v -t "$HOME" alacritty bspwm dunst font gtk hosts lock picom polybar \
-  resources rofi scripts sxhkd vis xdg xorg zsh
-
-# Create alias for .profile, because of LightDM.
-ln -sf "$HOME/.zprofile" "$HOME/.profile"
-
-# Source env variables for this install script as well
-# shellcheck source=$HOME/.zprofile
-. "$HOME/.zprofile"
+# ---------------------------------------------------------------------------------------------------------------------
 
 # Install dependencies
 echo "Installing dependencies.."
 sed -e '/^[[:space:]]*#/d' pkglist.txt |
   sed -E "s/^([a-z0-9-]+)\s*#.*$/\1/" |
   yay -S --needed -
+
+# Stow dotfiles.
+stow -v -t "$HOME" alacritty bspwm dunst font gtk hosts lock picom polybar pulseaudio resources rofi scripts spicetify \
+  sxhkd sxiv vis xdg xorg zathura zsh
+
+# Create alias for .profile, for compatibility with LightDM.
+ln -sf "$HOME/.zprofile" "$HOME/.profile"
+
+# Create home directories.
+DIRS=('.local' '.local/share' '.cache' '.config' '.ssh'
+  'desktop' 'docs' 'templates' 'dl' 'public' 'media' 'music' 'pictures' 'videos'
+  'docs/repo' 'pictures/screenshots'
+)
+
+for dir in $DIRS; do
+  mkdir -p "$HOME/$dir"
+done
+
+xdg-user-dirs-update
