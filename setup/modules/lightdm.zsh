d287befb1534edcbb4527c5000c20dd41dd4df6a
Dan Cîmpianu <dancristiancimpianu@gmail.com>
2020-08-17 21:46:42 +0300

    Set manjarowatch image as default login screen background.

--- setup/modules/lightdm.zsh
+++ setup/modules/lightdm.zsh
@@ -10,12 +10,12 @@ source "setup/modules/_modulebase.zsh" && moduleInit 'LightDM' || exit 126
 info "Setting up display manager."
 
 mkdir -p /usr/share/backgrounds
-#cp manjarowatch.png /usr/share/backgrounds || warn "Failed to copy background image resource."
 cp /etc/lightdm/lightdm.conf /etc/lightdm/lightdm-original.conf || warn "Couldn't backup the original lightdm conf."
+cp "$REPO_DIR/setup/resources/manjarowatch.png" /usr/share/backgrounds || warn "Failed to copy login background image."
 cp "$REPO_DIR/setup/resources/lightdm.conf" /etc/lightdm || error "Failed to copy lightdm.conf"
 cp "$REPO_DIR/setup/resources/slick-greeter.conf" /etc/lightdm || error "Failed to copy slick-greeter.conf."
 
-info "Enabling light-dm service..."
-systemctl --no-ask-password enable lightdm || warn "Could not enable lightdm service. Manual intervention required."
+info "Enabling LightDM service..."
+systemctl --no-ask-password enable lightdm || warn "Could not enable LightDM service. Manual intervention required."
 
 moduleDone
