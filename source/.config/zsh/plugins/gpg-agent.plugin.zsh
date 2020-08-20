2d5d110b62f2bc3f3d9e7788e1451013fc8b2bbe
Dan Cîmpianu <dancristiancimpianu@gmail.com>
2020-08-20 21:25:42 +0300

    GPG Agent
    * Added GPG config files.
    * Added zsh plugin for starting the GPG agent.
    * Removed old ssh-agent plugin.

---
+++ source/.config/zsh/plugins/gpg-agent.plugin.zsh
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# GPG Agent
+#
+# Sets the required environment variables for the GPG Agent and launches it if not already running.
+# ---------------------------------------------------------------------------------------------------------------------
+# shellcheck disable=SC2155
+export GPG_TTY="$(tty)"
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
