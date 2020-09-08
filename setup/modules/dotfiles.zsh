120afc69db2061d3a3402df4822d5f50d2536894
Dan <dancristiancimpianu@gmail.com>
2020-09-08 23:52:11 +0300

    Set safe permissions for ssh and gpg directories.
    After copying the files from the repository, restrict permissions.
    Otherwise, the agents will complain.

--- setup/modules/dotfiles.zsh
+++ setup/modules/dotfiles.zsh
@@ -23,6 +23,12 @@ DIR_PATHS=$(printf "/home/$INSTALL_USER/%s " "${DIRS[@]}")
 # shellcheck disable=SC2086
 install -dv -o "$INSTALL_USER" -g "$INSTALL_USER" -m 750 ${=DIR_PATHS} || warn "Could not create xdg-user-dirs."
 
+info 'Setting safe permissions for sensitive directories.'
+find "/home/$INSTALL_USER/.local/share/gnupg" -type d -exec chmod 700 {} \;
+find "/home/$INSTALL_USER/.local/share/gnupg" -type f -exec chmod 600 {} \;
+find "/home/$INSTALL_USER/.ssh" -type d -exec chmod 700 {} \;
+find "/home/$INSTALL_USER/.ssh" -type f -exec chmod 600 {} \;
+
 info "Copying over dotfiles."
 rsyncUser "./" "/home/$INSTALL_USER/docs/repo/dotfiles/" || warn "Could not save dotfiles copy."
 
