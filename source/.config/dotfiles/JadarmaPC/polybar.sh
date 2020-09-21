1da352e9ab305c1e8b535eb0dd5f84ca10d9a10d
Dan <dancristiancimpianu@gmail.com>
2020-09-21 21:21:26 +0300

    Revert display host override configs for JadarmaPC.
    This reverts commit cd0dba86, because I was not paying attention and did not blacklist nouveau (even though the `nvidia` package should have done that for me). Therefore, the old configs were the valid ones.
    Lessons learned.

--- source/.config/dotfiles/JadarmaPC/polybar.sh
+++ source/.config/dotfiles/JadarmaPC/polybar.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 # Polybar layout for my main machine, JadarmaPC
 # Main bar in center monitor, simple bar in side monitors.
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 
 MONITOR=DP-1 polybar --reload simple &
-MONITOR=DP-2 polybar --reload main &
-MONITOR=DP-3 polybar --reload simple &
+MONITOR=DP-3 polybar --reload main &
+MONITOR=DP-5 polybar --reload simple &
 unset MONITOR
--- source/.config/dotfiles/JadarmaPC/screen_layout.sh
+++ source/.config/dotfiles/JadarmaPC/screen_layout.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 # Screen layout for my main machine, JadarmaPC
 # Simple triple 1080p displays in landscape.
-# -----------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 xrandr \
-  --output HDMI-1 --off \
+  --output HDMI-0 --off \
+  --output DP-0 --off \
   --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal \
-  --output DP-2 --mode 1920x1080 --pos 1920x0 --rotate normal --primary \
-  --output DP-3 --mode 1920x1080 --pos 3840x0 --rotate normal \
+  --output DP-2 --off \
+  --output DP-3 --mode 1920x1080 --pos 1920x0 --rotate normal --primary \
+  --output DP-4 --off \
+  --output DP-5 --mode 1920x1080 --pos 3840x0 --rotate normal
