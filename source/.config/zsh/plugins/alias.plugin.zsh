75d13c36e9ee2b371d3fd8bbe4e422de13832cf8
Jadarma <dancristiancimpianu@gmail.com>
2022-11-07 18:14:04 +0200

    Create misc alias plugin.

---
+++ source/.config/zsh/plugins/alias.plugin.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+# ---------------------------------------------------------------------------------------------------------------------
+# MISC ALIASES
+#
+# A place to define extra aliases that don't fit in another category or make sense to be their own plugin.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Enable wayland backend for Webcord when starting from CLI.
+alias webcord='webcord --enable-features=UseOzonePlatform --ozone-platform=wayland'
